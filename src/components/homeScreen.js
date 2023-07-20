a754c37f4091ec0e85b6973f6fa65611368fb333
keerthanapalepu <keerthanapalepu2002@gmail.com>
2023-07-20 19:05:46 +0530

    bug fix at the horizontal scroll

--- src/components/homeScreen.js
+++ src/components/homeScreen.js
@@ -69,29 +69,24 @@ const useStyles = makeStyles((theme) => ({
 const HomeScreen = () => {
   const classes = useStyles();
   const { currentUser } = useAuth();
-  const [startIndex, setStartIndex] = useState(0);
   const [weekSongsArray, setWeekSongsArray] = useState([]);
-  const [lastAddedOn, setLastAddedOn] = useState(null);
+  const [lastCreatedOn, setLastCreatedOn] = useState(null);
   const [selectedCard, setSelectedCard] = useState(null);
   const [allSongsArray, setAllSongsArray] = useState([]);
   const [fav, setFav] = useState([]);
   const [cart, setCart] = useState([]);
+
   const handleNext = () => {
-    const nextIndex = startIndex + 4;
-    setStartIndex(nextIndex >= weekSongsArray.length ? 0 : nextIndex);
+    getSongs();
   };
 
-  const handlePrev = async () => {
-    const prevIndex = startIndex - 4;
-    setStartIndex(prevIndex < 0 ? weekSongsArray.length - 4 : prevIndex);
-  };
 
   const getSongs = async () => {
-    const collectionRef = collection(db, 'weeklyAlbum');
-    let firestoreQuery = query(collectionRef, orderBy('createdOn'), limit(8));
+    const collectionRef = collection(db, 'albums');
+    let firestoreQuery = query(collectionRef, orderBy('createdOn'), limit(2));
 
-    if (lastAddedOn) {
-      firestoreQuery = query(firestoreQuery, startAfter(lastAddedOn));
+    if (lastCreatedOn) {
+      firestoreQuery = query(firestoreQuery, startAfter(lastCreatedOn));
     }
 
     try {
@@ -117,7 +112,7 @@ const HomeScreen = () => {
             }
           }
         newDocs.push({ id: doc.id, url: imageUrl, day: doc.data().name, type: doc.data().type, theme: doc.data().theme, songs: doc.data().songsRef });
-        setLastAddedOn(doc.data().addedOn);
+        setLastCreatedOn(querySnapshot.docs[querySnapshot.docs.length - 1]);
       }));
 
       setWeekSongsArray((prevDocs) => [...prevDocs, ...newDocs]);
@@ -223,67 +218,35 @@ const HomeScreen = () => {
   
   return (
     <Grid container style={{ height: '85vh' }}>
-      <Grid
-        container
-        item
-        style={{
-          height: '45%',
-          backgroundColor: 'transparent',
-          margin: '0 10px 10px',
-          overflow: 'hidden', // Ensure overflow is hidden for the transition effect
-          position: 'relative', // Position relative for absolute positioning of cards
-          display: 'flex',
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-      <IconButton
-          className={classes.chevronLeft}
-          aria-label="Next"
-          onClick={handlePrev}
-        >
-          <BsChevronLeft />
-        </IconButton>
-        <div
-          style={{
-            width: `${weekSongsArray.length * 45}%`, // Adjust the width based on the number of weekSongsArray
-            transform: `translateX(-${startIndex * (100 / weekSongsArray.length)}%)`, // Translate the cards based on the start index
-            transition: 'transform 0.5s ease', // Apply the transition effect
-            display: 'flex',
-            marginLeft: '50px'
-          }}
-        >
-          {weekSongsArray.map((item, index) => (
-            <Grid
-          item
-          xs={12}
-          className={classes.gridItem}
-          key={index}
-        >
-          <Card className={classes.card} onClick={() => setSelectedCard(index)}>
-            <CardContent className={classes.cardContent}>
-              <div>
-                <img
-                  src={item.url}
-                  className={classes.image}
-                  alt={item.name}
-                />
-                <Typography className={classes.title}>{item.day}</Typography>
-                <Typography className={classes.theme}>{item.theme + "..."}</Typography>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
-          ))}
-        </div>
-        <IconButton
-          className={classes.chevron}
-          aria-label="Next"
-          onClick={handleNext}
-        >
-          <BsChevronRight />
-        </IconButton>
+      <div style={{ overflowX: 'auto', width: '100%' }}>
+  <div style={{ display: 'flex', flexWrap: 'nowrap', alignItems: 'flex-start' }}>
+    {weekSongsArray.map((item, index) => (
+      <Grid item xs={12} className={classes.gridItem} key={index}>
+        <Card className={classes.card} onClick={() => setSelectedCard(index)}>
+          <CardContent className={classes.cardContent}>
+            <div>
+              <img src={item.url} className={classes.image} alt={item.name} />
+              <Typography className={classes.title}>{item.day}</Typography>
+              <Typography className={classes.theme}>
+                {item.theme + '...'}
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
       </Grid>
+    ))}
+  </div>
+  <IconButton
+    className={classes.chevron}
+    aria-label="Next"
+    onClick={handleNext}
+  >
+    <BsChevronRight />
+  </IconButton>
+</div>
+
+
+
 
       <Grid container item style={{ height: '55%', backgroundColor: '#6A695E', margin: '0 10px 0' }}>
          <SongsList songs={allSongsArray} setAllSongsArray={setAllSongsArray} day={weekSongsArray[selectedCard]?.type}/>
